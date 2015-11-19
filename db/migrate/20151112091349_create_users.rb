f9cc7c50a6b1a3683cc3632d2f20027af1c83880
kosakunitanda <k.nitanda812@gmail.com>
2015-11-19 11:48:24 +0000

    migrate

--- db/migrate/20151112091349_create_users.rb
+++ db/migrate/20151112091349_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.string :area
       t.string :age
 
       t.timestamps null: false
